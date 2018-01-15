7fd426fac48aeb4236fac842929aa98daf6c217d
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2018-01-15 22:10:48 +0200

    add slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,31 +3,38 @@ import { NavController } from 'ionic-angular';
 import { ImagePicker } from '@ionic-native/image-picker';
 import { Camera } from '@ionic-native/camera';
 import { clamp } from 'ionic-angular/util/util';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public pictures: string;
-  public pictureSrc: string;
-  public bgImage: string;
-  public base64Image: any;
+  public scenes: any[];
+  @ViewChild(Slides) slides: Slides;
 
   constructor(
     public navCtrl: NavController,
     public camera: Camera,
   ) {
-    this.pictureSrc = 'https://facebook.github.io/react-native/img/header_logo.png';
-    this.bgImage = `url('${this.pictureSrc}');`;
+    this.scenes = [];
+    this.scenes.push({});
   }
 
-  async click() {
+  async addScene() {
+    this.scenes.push({});
+    setTimeout(() => this.slides.slideTo(this.slides.length() - 1, 500), 100);
+  }
+
+  async addImage(scene) {
     const imageData = await this.camera.getPicture({
       sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
       destinationType: this.camera.DestinationType.DATA_URL
     });
 
-    this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    const base64Image = `data:image/jpeg;base64,${imageData}`;
+
+    scene.base64Image = base64Image;
   }
 }
