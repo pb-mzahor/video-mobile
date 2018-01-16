734b8ff36f6f4d1d75f1a331465fed30a201faba
Yaniv Goltshian <yaniv@playbuzz.com>
2018-01-16 13:30:18 +0200

    add play video side

---
+++ src/pages/play/play.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-play',
+  templateUrl: 'play.html'
+})
+export class PlayPage {
+
+  constructor(
+    public navCtrl: NavController
+  ) {
+
+  }
+
+  async saveVideo() {
+
+  }
+}
