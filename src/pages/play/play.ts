26bcd437d0a9305536aaad2026c9b052cc60445f
Yaniv Goltshian <yaniv@playbuzz.com>
2018-01-17 20:12:51 +0200

    add crosspost btn

--- src/pages/play/play.ts
+++ src/pages/play/play.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'page-play',
@@ -7,13 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class PlayPage {
 
-  constructor(
-    public navCtrl: NavController
-  ) {
+  constructor(private http: HttpClient) { }
 
-  }
 
   async saveVideo() {
+  }
 
+  async crosspost() {
+    let videoUrl = 'http://img.playbuzz.com/video/upload/w_200,h_720,g_center,c_crop/up0szhaqpakhv4dnzpkz.mp4';
+    const url = `https://stg-video-creator.playbuzz.com/mobile/crossPostVideo`;
+    let fd = new FormData();
+    fd.append('videoUrl', videoUrl);
+    fd.append('facebookPageUrl', 'https://www.facebook.com/yosefKaro7TLV');
+    fd.append('sectionId', '79084e72-1125-4d37-a6a9-62837ca660f9');
+    fd.append('itemId', '12277a5f-d3e7-4c3e-8614-a13a23c3f0c4');
+
+    const response: any = await this.http.post(url, fd).toPromise();
+
+    return response.secure_url;
   }
 }
--- src/services/CloudinaryService.ts
+++ src/services/CloudinaryService.ts
@@ -15,9 +15,11 @@ export class CloudinaryService {
         fd.append('upload_preset', unsignedUploadPreset);
         fd.append('tags', 'browser_upload');
         fd.append('file', base64Image);
-        
+
         const response: any = await this.http.post(url, fd).toPromise();
-        
-        return response.secure_url;
+
+        var redicertToFbUrl = response.videosYouCanCrosspostURL;
+        window.open(redicertToFbUrl, '_system');
+        return response.success;
     }
 }
